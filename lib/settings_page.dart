85636b24202b5d92ba5a9da57368c83d82074af0
Elnaam10 <e.umutoni@alustudent.com>
2024-07-28 17:51:49 -0400

    settings

--- lib/settings_page.dart
+++ lib/settings_page.dart
@@ -14,7 +14,29 @@ class SettingsPage extends StatelessWidget {
           _buildSettingOption(context, 'Currency', 'RWF'),
           _buildSettingOption(context, 'Language', 'English'),
           _buildSettingOption(context, 'Theme', 'Dark'),
-          _buildS
+          _buildSettingOption(context, 'Security', 'Face ID'),
+          _buildSettingOption(context, 'Notification', ''),
+          _buildSettingOption(context, 'About', ''),
+          _buildSettingOption(context, 'Help', ''),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSettingOption(
+      BuildContext context, String title, String subtitle) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      elevation: 4,
+      margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+      child: ListTile(
+        title: Text(title,
+            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500)),
+        subtitle: subtitle.isNotEmpty ? Text(subtitle) : null,
+        trailing: const Icon(Icons.arrow_forward_ios, size: 18),
+        onTap: () {
           // Add navigation to setting detail pages if necessary
         },
       ),
