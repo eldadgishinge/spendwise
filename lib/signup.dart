2c2ffa85dc50fa907534c3fc685a2776354e8815
eldadgishinge <e.gishinge@alustudent.com>
2024-07-28 23:21:18 +0200

    signup

--- lib/signup.dart
+++ lib/signup.dart
@@ -19,7 +19,114 @@ class SignUpPageState extends State<SignUpPage> {
   bool _termsAccepted = false;
   String _errorMessage = '';
 
-  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text('Sign Up'),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        iconTheme: const IconThemeData(color: Colors.black),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            _buildTextField('Name', _nameController),
+            _buildTextField('Email', _emailController),
+            _buildTextField('Password', _passwordController, isPassword: true),
+            const SizedBox(height: 20),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Checkbox(
+                  value: _termsAccepted,
+                  onChanged: (bool? value) {
+                    setState(() {
+                      _termsAccepted = value ?? false;
+                    });
+                  },
+                ),
+                const Text(
+                  'By signing up, you agree to the ',
+                  style: TextStyle(fontWeight: FontWeight.w500),
+                ),
+                const Text(
+                  'Terms of Service and Privacy Policy',
+                  style: TextStyle(
+                      color: Colors.blue, fontWeight: FontWeight.w500),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _termsAccepted ? _signUp : null,
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size(double.infinity, 50),
+                backgroundColor: const Color(0xFF00B2E7),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+              ),
+              child:
+                  const Text('Sign Up', style: TextStyle(color: Colors.white)),
+            ),
+            const SizedBox(height: 20),
+            if (_errorMessage.isNotEmpty)
+              Padding(
+                padding: const EdgeInsets.only(top: 20.0),
+                child: Text(
+                  _errorMessage,
+                  style: const TextStyle(color: Colors.red),
+                ),
+              ),
+            const SizedBox(height: 20),
+            const Text('Or continue with'),
+            const SizedBox(height: 20),
+            ElevatedButton.icon(
+              onPressed: () {
+                // Implement Google Sign-Up logic here
+              },
+              icon: Image.asset(
+                'assets/google-icon.png',
+                width: 24, // Specify the size of the icon
+                height: 24,
+              ),
+              label: const Text('Sign Up with Google'),
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size(double.infinity, 50),
+                backgroundColor: Colors.white,
+                foregroundColor: Colors.black,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            GestureDetector(
+              onTap: () {
+                Navigator.pushNamed(context, '/login');
+              },
+              child: const Text(
+                'Already have an account? Login',
+                style: TextStyle(color: Colors.blue),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTextField(String label, TextEditingController controller,
+      {bool isPassword = false}) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10.0),
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+          labelText: label,
           labelStyle: TextStyle(color: Colors.grey[500], fontSize: 14.0),
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(12.0),
